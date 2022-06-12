b5d89bf0b14ec63aa39b79a0a340bf61fa9dd4e8
Christina Webber <chrisswebb@yahoo.com>
2022-06-11 20:05:50 -0500

    trying temperature as class

--- src/index.js
+++ src/index.js
@@ -1,20 +1,65 @@
-const state = {
-    temp: 78,
-    units: '°F',
-    // sky: clear
+const axios = require('axios');
+const mySecret = os.env['api_key'];
+const pageTemp = document.querySelector("#temp");
+
+class Temp {
+    constructor() {
+        this.number = 78;
+        this.unit = '°F';
+        this.city = 'Dallas';
+    }
+    
+    increaseTemp() {
+        this.number += 1;
+        pageTemp.textContent = `${this.number} ${this.units}`;
+    };
+
+    decreaseTemp() {
+        this.number -= 1;
+        pageTemp.textContent = `${state.temp} ${state.units}`;
+    };
+
+    actualTemp(city) {
+        this.city = city;
+        this.coordinates = getCoordinates(city);
+        this.number = 0;
+    };
+
+    getCoordinates(placeName) {
+        return axios
+        .get('https://us1.locationiq.com/v1/search.php', {
+            params: {
+            key: mySecret,
+            q: placeName,
+            format: 'json',
+            },
+        })
+        .then(response => {
+            const lat = response.data[0].lat;
+            const lon = response.data[0].lon;
+
+            return { lat, lon };
+        })
+    };
 };
 
-const increaseTemp = () => {
-    state.temp += 1;
-    const temperature = document.querySelector("#temp");
-    temperature.textContent = `${state.temp} ${state.units}`;
-};
+// const increaseTemp = () => {
+//     state.temp += 1;
+//     const temperature = document.querySelector("#temp");
+//     temperature.textContent = `${state.temp} ${state.units}`;
+// };
 
-const decreaseTemp = () => {
-    state.temp -= 1;
-    const temperature = document.querySelector("#temp");
-    temperature.textContent = `${state.temp} ${state.units}`;
-};
+// const decreaseTemp = () => {
+//     state.temp -= 1;
+//     const temperature = document.querySelector("#temp");
+//     temperature.textContent = `${state.temp} ${state.units}`;
+// };
+
+// const changeCity = () => {
+//     const city = document.querySelector("#current-city");
+//     state.city = input;
+//     city.textContent = `Currently displaying weather in ${state.city}`
+// };
 
 // const actualTemp = () => {
 //     // make API call here pluggin in state.city
@@ -32,15 +77,13 @@ const decreaseTemp = () => {
 //     }
 // };
 
-// const changeCity = () => {
-//     state.city = `${input}`;
-// };
-
 const registerEventHandlers = (event) => {
     const increaseTempButton = document.getElementById("up");
-    increaseTempButton.addEventListener("click", increaseTemp);
+    increaseTempButton.addEventListener("click", state.temperature.increaseTemp);
     const decreaseTempButton = document.getElementById("down");
-    decreaseTempButton.addEventListener("click", decreaseTemp);
+    decreaseTempButton.addEventListener("click", state.temperature.decreaseTemp);
+    const changeCityButton = document.getElementById("find");
+    changeCityButton.addEventListener("click", state.temperature.changeCity)
     // const changeSkySelector = document.getElementById("Select");
     // changeSkySelector.addEventListener('click', changeSky);
 };
